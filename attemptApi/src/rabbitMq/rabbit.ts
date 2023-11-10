522e0cc2e291f52d09ad8947175161e661609061
9fc70c892 <89006592+9fc70c892@users.noreply.github.com>
2023-11-10 15:33:06 +0800

    Include ENV for rabbit

--- attemptApi/src/rabbitMq/rabbit.ts
+++ attemptApi/src/rabbitMq/rabbit.ts
@@ -1,1 +1,46 @@
-import amqp from 'amqplib';
+/**
+ * @file For RabbitMQ access.
+ */
+// eslint-disable-next-line @typescript-eslint/naming-convention
+import RoomServiceMqConsumer, { RoomEvent } from './room_service_mq_consumer';
+
+/**
+ *
+ */
+async function consumerClassExample(): Promise<void> {
+  // Configs for MQ client
+  // It is recommended that you set these via environment variables
+  const consumer: RoomServiceMqConsumer = new RoomServiceMqConsumer({
+    password: 'P@ssword123',
+    user: 'user',
+    host: 'localhost',
+    port: 5672,
+    vhost: '', // Vhost is a non-empty string in production
+    shouldUseTls: false, // TLS will be used during production
+    exchangeName: 'room-events',
+
+    // This is completely up to you. I recommend the format
+    // "xxx-service-room-event-queue".
+    // By specifying a queue name, you can connect back to the queue and resume
+    // from where you left off should a disconnect from the MQ occur.
+    queueName: 'editor-service-room-event-queue',
+  });
+
+  // Ensures setup is complete
+  await consumer.initialise();
+
+  // Consume messages by calling the specified  callback every time a message is
+  // received
+  await consumer.consume(async (data: RoomEvent) => {
+    // Note that the "consume" method renames the event's field names to camel
+    // case.
+    console.log(`Received: ${JSON.stringify(data)}`);
+  });
+}
+
+// TODO: Uncomment this to see a simple example
+// simpleExample();
+
+// TODO: Uncomment this to see the example which utilises my
+// RoomServiceMqConsumer class
+// consumerClassExample();
